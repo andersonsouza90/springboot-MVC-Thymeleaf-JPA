7fa8a53d304476466387fa557492808079294105
andersonsouza90 <andersonsilva.souza90@gmail.com>
2021-04-05 22:49:31 -0300

    Editando e excluindo cadastro - GetMapping - PathVariable

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,8 +1,12 @@
 package curso.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,19 +24,23 @@ public class PessoaController {
 	public ModelAndView inicio() {
 		
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoaobj", new Pessoa());
+		
 		Iterable<Pessoa> pessoaIt = pessoaRepository.findAll();
 		andView.addObject("pessoas", pessoaIt);
+		
 		
 		return andView;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
 	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 		
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
 		Iterable<Pessoa> pessoaIt = pessoaRepository.findAll();
 		andView.addObject("pessoas", pessoaIt);
+		andView.addObject("pessoaobj", new Pessoa());
 		
 		return andView;
 	}
@@ -42,6 +50,31 @@ public class PessoaController {
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
 		Iterable<Pessoa> pessoaIt = pessoaRepository.findAll();
 		andView.addObject("pessoas", pessoaIt);
+		andView.addObject("pessoaobj", new Pessoa());
 		return andView;
+	}
+	
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		Optional<Pessoa> p = pessoaRepository.findById(idpessoa);
+		modelAndView.addObject("pessoaobj", p.get());
+		return modelAndView;
+		
+	}
+	
+	@GetMapping("/excluirpessoa/{idpessoa}")
+	public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {
+		
+		ModelAndView MV = new ModelAndView("cadastro/cadastropessoa");
+		
+		pessoaRepository.deleteById(idpessoa);	
+		
+		Iterable<Pessoa> p = pessoaRepository.findAll();
+		MV.addObject("pessoas", p);
+		MV.addObject("pessoaobj", new Pessoa());
+		
+		return MV;
 	}
 }
